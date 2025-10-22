83c194e9fbc610d6b81ffb2c5ecbe72a1bb7bdc0
aliciaggz <aliciagallegonz@gmail.com>
2025-10-22 18:56:49 +0200

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import path from 'path';
 
+const isProd = process.env.NODE_ENV === 'production';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  base: isProd ? '/andrea-portfolio' : '/',
+  site: isProd ? 'https://aliciaggz.github.io/andrea-portfolio' : 'http://localhost:4321',
   vite: {
     resolve: {
       alias: {
