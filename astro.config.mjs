bb1de47312a35b465ea225570f6ecb6538868c9f
aliciaggz <aliciagallegonz@gmail.com>
2025-11-10 17:50:55 +0100

    fix to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import path from 'path';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import path from "path";
 
-const isProd = process.env.NODE_ENV === 'production';
-
+const isVercel = process.env.VERCEL === "1";
+const isProd = process.env.NODE_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  base: isProd ? '/andrea-portfolio' : '/',
-  site: isProd ? 'https://aliciaggz.github.io/andrea-portfolio' : 'http://localhost:4321',
+  base: isVercel ? "/" : "/andrea-portfolio",
+  site: isVercel
+    ? "https://andrea-portfolio.vercel.app"
+    : "https://aliciaggz.github.io/andrea-portfolio",
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve('./src')
-      }
+        "@": path.resolve("./src"),
+      },
     },
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: `
           @use "@/styles/variables" as *; 
-                 `
-      },
+                 `,
+        },
       },
     },
   },
